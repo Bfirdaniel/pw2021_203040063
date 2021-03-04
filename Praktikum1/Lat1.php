952f51535e243db314e544c534eaa8c008c8bce2
bfirdaniel <bfirdaniel@gmail.com>
2021-03-04 18:09:34 +0700

    Praktikum 1

---
+++ Praktikum1/Lat1.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Billy Firdaniel Nafitra
+203040063
+https://github.com/Bfirdaniel/pw2021_203040063
+Praktikum Jum'at 10:00-11:00
+*/
+?>
+
+
+<?php
+for( $i = 1; $i < 4; $i++ ) {
+    for( $b = 1; $b < 4; $b++ ) {
+        echo "Ini pengulangan ke ($i,$b)<br>";
+    }
+}
+?>
